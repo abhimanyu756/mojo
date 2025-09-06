cd7dbafbc707bbce48f40713dabcf5bdbb1503fa
ChetanTiwari195 <chetan56tiwari@gmail.com>
2025-09-06 16:56:08 +0530

    last commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,7 @@ function App() {
               <Route path="/cart" element={<Cart />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/orders" element={<OrderHistory />} />
+
             </Routes>
           </main>
         </div>
